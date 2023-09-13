98d2719e8fd0d16879cdd224919fc1436a8e07f3
newbiebaby7919 <131728576+newbiebaby7919@users.noreply.github.com>
2023-09-13 20:40:42 +0800

    Update and rename README.md to try.sh

---
+++ try.sh
@@ -0,0 +1,21 @@
+mkdir A B C D E
+
+cd A
+touch file{1..3}.txt
+cd ..
+
+cd B
+touch file{4..6}.txt
+cd ..
+
+cd C
+touch file{7..9}.txt
+cd ..
+
+cd D
+touch file{10..12}.txt
+cd ..
+
+cd E
+touch file{13..15}.txt
+cd ..
